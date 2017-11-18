6cc8b7827a42a2f4fcabd51b7b0134157acb9ae5
Your Name <andre.barros@outlook.com>
2017-11-18 20:26:49 -0200

    Criação camadas

--- src/SysChief.Infra.CrossCutting.IoC/obj/Debug/netcoreapp2.0/SysChief.Infra.CrossCutting.IoC.AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using System.Reflection;
-
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyTitleAttribute("SysChief.Infra.CrossCutting.IoC")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
-
